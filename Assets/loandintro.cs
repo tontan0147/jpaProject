3715d26251af3e83ae31129972ef27fe37cd8fea
tontan0147 <132797555+tontan0147@users.noreply.github.com>
2024-02-08 18:56:38 +0700

    update

---
+++ Assets/loandintro.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loandintro : MonoBehaviour
+{
+   
+    public string nextSceneName;
+
+    void Start()
+    {
+        // Start the coroutine when the script is initialized
+        StartCoroutine(TransitionCoroutine());
+    }
+
+    IEnumerator TransitionCoroutine()
+    {
+
+        // Wait for the animation to complete
+        yield return new WaitForSeconds(2.0f);
+
+        // Load the next scene
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+}
